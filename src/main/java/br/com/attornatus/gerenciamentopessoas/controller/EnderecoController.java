c45aa6ced1b5a39107685d46acbc6e701afd59bc
Marcklen <marcklen@icloud.com>
2023-01-02 16:31:44 -0300

    feat: criando listagem de enderecos

--- src/main/java/br/com/attornatus/gerenciamentopessoas/controller/EnderecoController.java
+++ src/main/java/br/com/attornatus/gerenciamentopessoas/controller/EnderecoController.java
@@ -2,9 +2,12 @@ package br.com.attornatus.gerenciamentopessoas.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.attornatus.gerenciamentopessoas.model.dto.EnderecoDTO;
 import br.com.attornatus.gerenciamentopessoas.service.EnderecoService;
 
 @RestController
@@ -13,7 +16,19 @@ public class EnderecoController {
 
 	@Autowired
 	private EnderecoService enderecoService;
-	
+
 	@Autowired
 	private ModelMapper modelMapper;
+
+	@GetMapping
+	public ResponseEntity<?> findAllEnderecos() {
+		try {
+			var enderecoList = enderecoService.findAll();
+			var resultado = enderecoList.stream().map(endereco -> modelMapper.map(endereco, EnderecoDTO.class))
+					.toList();
+			return ResponseEntity.ok(resultado);
+		} catch (RuntimeException e) {
+			return ResponseEntity.internalServerError().build();
+		}
+	}
 }
