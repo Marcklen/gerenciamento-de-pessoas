21c279730032ce5dc674b23259f79084a45600f7
Marcklen <marcklen@icloud.com>
2023-01-02 20:44:49 -0300

    test: criando classes de testes

---
+++ src/test/java/br/com/attornatus/gerenciamentopessoas/controller/EnderecoControllerTest.java
@@ -0,0 +1,19 @@
+package br.com.attornatus.gerenciamentopessoas.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EnderecoControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindAllEnderecos() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/gerenciamentopessoas/controller/PessoaControllerTest.java
@@ -0,0 +1,39 @@
+package br.com.attornatus.gerenciamentopessoas.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PessoaControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindAllPessoas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByIdPessoa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateEndereco() {
+		fail("Not yet implemented");
+	}
+
+}
