b8eb4d6ce962698f9d81401e38ce788a56078666
Marcklen <marcklen@icloud.com>
2023-01-02 13:27:19 -0300

    feat: Model Mapper para facilitar o mapeamento entre as entidades

---
+++ src/main/java/br/com/attornatus/gerenciamentopessoas/config/ModelMapper.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.gerenciamentopessoas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapper {
+
+	@Bean
+	public ModelMapper getMapper() {
+		return new ModelMapper();
+	}
+}
