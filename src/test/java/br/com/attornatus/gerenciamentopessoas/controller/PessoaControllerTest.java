1fe028125b1a4fe8a5aae9369068d7981bd79dde
Marcklen <marcklen@icloud.com>
2023-01-03 13:24:14 -0300

    test: iniciando os testes do controlador de pessoas

--- src/test/java/br/com/attornatus/gerenciamentopessoas/controller/PessoaControllerTest.java
+++ src/test/java/br/com/attornatus/gerenciamentopessoas/controller/PessoaControllerTest.java
@@ -4,9 +4,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+
+import br.com.attornatus.gerenciamentopessoas.model.dto.EnderecoCreateDTO;
+import br.com.attornatus.gerenciamentopessoas.service.PessoaService;
 
 class PessoaControllerTest {
-
+	
+	@InjectMocks
+	private PessoaController controller;
+	
+	@Mock
+	private ModelMapper mapper;
+	
+	@Mock
+	private PessoaService service;
+	
+	@Mock
+	private EnderecoCreateDTO enderecoCreateDTO;
+	
 	@BeforeEach
 	void setUp() throws Exception {
 	}
